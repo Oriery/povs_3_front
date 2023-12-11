f2559f779865abf73d935f3a48fe84970a6b2d93
Oriery <panev291002@gmail.com>
2023-12-11 10:10:30 +0300

    fix for Windows

--- src/unimport.d.ts
+++ src/unimport.d.ts
@@ -1,16 +1,16 @@
 export {}
 declare global {
-  const pushNotify: typeof import('src/main')['pushNotify']
+  const pushNotify: typeof import('C:\\Users\\Aleksandr\\Desktop\\povs_3_front\\src\\main')['pushNotify']
 }
 // for vue template auto import
 import { UnwrapRef } from 'vue'
 declare module 'vue' {
   interface ComponentCustomProperties {
-    readonly pushNotify: UnwrapRef<typeof import('src/main.ts')['pushNotify']>
+    readonly pushNotify: UnwrapRef<typeof import('C:\\Users\\Aleksandr\\Desktop\\povs_3_front\\src\\main.ts')['pushNotify']>
   }
 }
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
-    readonly pushNotify: UnwrapRef<typeof import('src/main.ts')['pushNotify']>
+    readonly pushNotify: UnwrapRef<typeof import('C:\\Users\\Aleksandr\\Desktop\\povs_3_front\\src\\main.ts')['pushNotify']>
   }
 }
